c7d70d0d42d57f41eeb727e45482fb77d1df17c8
chaopengz <cpz@pku.edu.cn>
2019-01-21 17:50:53 +0800

    add allow host

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!hpfrw%f2%h1evbuk*147#yit=cwz67*@f(v%p*63hsa=64f=z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.80.14.232']
 
 
 # Application definition
