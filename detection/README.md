9a2976bfacddf79e04a340af013f17ea6efd29cc
chaopengz <cpz@pku.edu.cn>
2019-01-22 17:45:54 +0800

    add yolo detection

---
+++ detection/README.md
@@ -0,0 +1,2 @@
+### OneStageDet
+Now we have implemented yolov2 and yolov3 in the `yolo` folder, in the future we consider to implement yolo and ssd in a single framework. 
--- web/app/views.py
+++ web/app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+# coding:utf-8
+from django.shortcuts import render, render_to_response, redirect
+from django.http import *
+from django.template import loader, context, RequestContext
+import json
+from django.http import JsonResponse
+import os
+from app.models import *
+import json
+
+
+def query_items():
+    '''
+    1: compute similarity
+    2: sort scores
+    3: return top N result in json format
+    :return:
+    '''
+    pass
+
+
+def query_imgs():
+    pass
+
+
+def get_recommended():
+    pass
