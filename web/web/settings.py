4a3f5edc3ce87e10be660ded651b8fd6dbe45cd0
chaopengz <cpz@pku.edu.cn>
2019-01-22 09:58:36 +0800

    add static

--- web/web/settings.py
+++ web/web/settings.py
@@ -48,7 +48,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'web.urls'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -111,4 +114,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
