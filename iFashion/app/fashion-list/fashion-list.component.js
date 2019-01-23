721d5be1f3a33689385e50ef68eeffcefe75bade
HuYang719 <646228006@qq.com>
2019-01-23 21:01:22 +0800

    update ifashion

--- iFashion/app/fashion-list/fashion-list.component.js
+++ iFashion/app/fashion-list/fashion-list.component.js
@@ -17,9 +17,16 @@ angular.
 
     $scope.submit = function() {
     $scope.form.image = $scope.files[0];
+
+
+
+    // var blob = new Blob(byteArrays, { type: contentType });
+    // var file = new File([blob], filename, {type: contentType, lastModified: Date.now()});
+
+
     $http({
     method  : 'POST',
-    url     : '',
+    url     : 'http://127.0.0.1:8001/post/',
     processData: false,
     transformRequest: function (data) {
         var formData = new FormData();
@@ -28,7 +35,7 @@ angular.
     },  
         data : $scope.form,
         headers: {
-               'Content-Type': undefined
+               'Content-Type': 'multipart/form-data'
         }
         }).success(function(data){
             //alert(data);
@@ -36,10 +43,45 @@ angular.
 
     };
 
+    // src is under the url/
+    function srcToFile(src, fileName, mimeType){
+      return (fetch(src)
+          .then(function(res){return res.arrayBuffer();})
+          .then(function(buf){return new File([buf], fileName, {type:mimeType});})
+      );
+    }
+
+    //Test Button
+    $scope.TestButn = function(){
+
+      // 0-fecth img
+      // 1-convert img to a file
+      // 2-tranform to formData
+      srcToFile("./test.jpg", 'new.png', 'image/png')
+      .then(function(file){
+          var fd = new FormData();
+          fd.append('image', file);
+          return fetch('http://127.0.0.1:8001/post/', {method:'POST', body:fd});
+      })
+      .then(function(res){
+          return res.text();
+      })
+      .then(console.log)
+      .catch(console.error); 
+
+      $scope.PostData = function(){
+        $http.get('http://127.0.0.1:8001/post/').then(function(response) {
+            self.fashions = response.data;
+        });
+      }
+
+    };
+
+
+    // preview data
     $scope.uploadedFile = function(element) {
     $scope.currentFile = element.files[0];
     var reader = new FileReader();
-
 
     reader.onload = function(event) {
       $scope.image_source = event.target.result
@@ -48,44 +90,59 @@ angular.
       });
     }
     reader.readAsDataURL(element.files[0]);
-  }
+  };
+
 
   // User choose image
   $scope.chooseimg = function(){
-    var cimg = new Image();
-    cimg.src = this.fashion.imageUrl;
-    alert(cimg.src);
 
-    // $http({
-    // method  : 'POST',
-    // url     : '',
-    // processData: false,
-    // transformRequest: function (data) {
-    //     var formData = new FormData();
-    //     formData.append("image",cimg);  
-    //     return formData;  
-    // },  
-    //     data : $scope.form,
-    //     headers: {
-    //            'Content-Type': undefined
-    //     }
-    //     }).success(function(data){
-    //         //alert(data);
-    //     });
+    // get the user choose image 
+    // trans to the file
+    srcToFile(this.fashion.imageUrl, this.fashion.image_id, 'image/png')
+    .then(function(file){
+        var fd = new FormData();
+        fd.append('image', file);
+        //return fetch('http://127.0.0.1:8001/post/', {method:'POST', body:fd});
+        return fetch('http://34.80.196.77/post/', {method:'POST', body:fd});   
+        
+    })
+    .then(function(response){
+        var newfashions= [];
+        response.json().then(function(data) {
 
-      $http.get('fashions/testRecive.json').then(function(response) {
-          self.fashions = response.data;
-        });
+          // do something with your data
+        console.log(data.length);
+        for(var i = 0; i < data.length; i++) {
+          // edit the url for img
+          newfashions.push(data[i]);
+        }
+        // self.fashions = newfashions;
+        console.log(self.fashions);
+        self.fashions = newfashions;
+        $scope.$apply();
+        console.log(self.fashions);
+        console.log(data[0].imageUrl);
+          // $scope.fashion.imageUrl = data[3].imageUrl;
+        }); 
+  
+    })
+    .then(console.log)
+    .catch(console.error);
 
-      $scope.h_1 = "I guess you will ❤️...";
+  // $http.get('fashions/testRecive.json').then(function(response) {
+  //     self.fashions = response.data;
+  //   });
+ 
+
+  $scope.h_1 = "I guess you will ❤️...";
 
   }
 
-  $scope.PostData = function(){
-    $http.get('fashions/testRecive.json').then(function(response) {
-        self.fashions = response.data;
-      });
-  }
+  // $scope.PostData = function(){
+  //   $http.get('http://34.80.200.205/post/').then(function(response) {
+  //       self.fashions = response.data;
+  //     });
+  // }
 
   // sidebar
   $scope.openNav = function(){
--- iFashion/app/fashion-upload/fashion-upload.component.js
+++ iFashion/app/fashion-upload/fashion-upload.component.js
@@ -5,39 +5,45 @@ angular.
   module('fashionUpload').
   component('fashionUpload', {
     templateUrl: 'fashion-upload/fashion-upload.template.html',
-    controller: function FashionUploadController($scope, $http) {
+    controller: function FashionUploadController($scope, $http,$location) {
       var self = this;
       $http.get('fashions/fashion.json').then(function(response) {
         self.fashions = response.data;
       });
 
-    $scope.form = [];
-    $scope.files = [];
+      $scope.form = [];
+      $scope.files = [];
+      var form = document.getElementById("uploadform");
 
+      function sendData() {
+        var XHR = new XMLHttpRequest();
 
-    $scope.submit = function() {
-    $scope.form.image = $scope.files[0];
-    $http({
-    method  : 'POST',
-    url     : '',
-    processData: false,
-    transformRequest: function (data) {
-        var formData = new FormData();
-        formData.append("image", $scope.form.image);  
-        return formData;  
-    },  
-        data : $scope.form,
-        headers: {
-               'Content-Type': undefined
-        }
-        }).success(function(data){
-            //alert(data);
+        // Bind the FormData object and the form element
+        var FD = new FormData(form);
+
+        // Define what happens on successful data submission
+        XHR.addEventListener("load", function(event) {
+          // alert(event.target.responseText);
         });
 
-    };
+        // Define what happens in case of error
+        XHR.addEventListener("error", function(event) {
+          alert('Oops! Something went wrong.');
+        });
+
+        // Set up our request
+        // XHR.open("POST", "http://34.80.200.205/post/");
+        XHR.open("POST", "http://127.0.0.1:8001/post/");
+        
+        // The data sent is what the user provided in the form
+        XHR.send(FD);
+  };
+
+
 
     $scope.uploadedFile = function(element) {
     $scope.currentFile = element.files[0];
+    //$scope.currentFile.name = "image";
     var reader = new FileReader();
 
 
@@ -48,12 +54,11 @@ angular.
       });
     }
     reader.readAsDataURL(element.files[0]);
-  }
+  };
 
-  $scope.PostData = function(){
-    $http.get('fashions/testRecive.json').then(function(response) {
-        self.fashions = response.data;
-      });
+  $scope.UploadImg = function(){
+      sendData();
+      $location.path('/fashions').replace()
   }
 
   // sidebar
