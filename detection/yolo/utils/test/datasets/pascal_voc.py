43a070aef6027537b9c6028f40e4ff617e45ef15
chaopengz <cpz@pku.edu.cn>
2019-01-23 10:37:23 +0800

    change to multi class, add fashion class

--- detection/yolo/utils/test/datasets/pascal_voc.py
+++ detection/yolo/utils/test/datasets/pascal_voc.py
@@ -27,8 +27,12 @@ class pascal_voc(imdb):
         self._devkit_path = '/data2/yichaoxiong/data/VOCdevkit'
         self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
         print(self._data_path)
-        self._classes = ('__background__', # always index 0
-                         'fashion')
+        self._classes = ('__background__',  # always index 0
+                         'aeroplane', 'bicycle', 'bird', 'boat',
+                         'bottle', 'bus', 'car', 'cat', 'chair',
+                         'cow', 'diningtable', 'dog', 'horse',
+                         'motorbike', 'person', 'pottedplant',
+                         'sheep', 'sofa', 'train', 'tvmonitor','fashion')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
